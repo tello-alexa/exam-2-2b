932dfc4346753f07dda4325759005ec7d733eb70
Alexa Tello <tello.alexa@yahoo.com>
2022-11-10 00:54:57 -0600

    a

--- burgers_and_fries.cpp
+++ burgers_and_fries.cpp
@@ -3,6 +3,8 @@
 #include <semaphore.h>
 #include <mutex>
 #include <unistd.h>
+#include <vector>
+#include <algorithm>
 using namespace std;
 #define MAX_THREADS 100
 
@@ -12,6 +14,10 @@ const char* type_names[] = {"BURGER", "FRIES"};
 #define pii pair<int, int>
 
 int k;
+sem_t sem;
+//int type_to_check = -1;
+vector<int> waiting;
+mutex m[2];
 
 // Do not change
 void process_order() {
@@ -19,24 +25,32 @@ void process_order() {
 }
 
 void place_order(int type) {
-    /**
-     *  Add logic for synchronization before order processing
-     *  Check if already k orders in process; 
-     *     if true -> wait (print waiting)
-     *     otherwise place this order (print order)
-     *  Use type_names[type] to print the order type
-     */
+    // get number of orders currently processing
+    int value;
+    sem_getvalue(&sem, &value);
 
+    // wait if k orders are processing
+    if(value == 0) {
+        cout << "Waiting: " << type_names[type] << endl;
+        waiting.push_back(type);
+        //m[type].lock();
+    }
+    sem_wait(&sem);
+
+    // place and process order
+    cout << "Order: " << type_names[type] << endl;
     process_order();        // Do not remove, simulates preparation
 
-    /**
-     *  Add logic for synchronization after order processed
-     *  Allow next order of the same type to proceed if there is any waiting; if not, allow the other type to proceed.
-     */
+    // check for waiting orders of same type
+    /*int index = *find(waiting.begin(), waiting.end(), type);
+    if(index != *waiting.end()) {
+        m[type].unlock();
+    }*/
+    sem_post(&sem);
+
 }
 
 int main() {
-    // Initialize necessary variables, semaphores etc.
     
     // Read data: done for you, do not change
     pii incoming[MAX_THREADS];
@@ -49,6 +63,9 @@ int main() {
         incoming[i].first = _type;
         incoming[i].second = _arrival;
     }
+
+    // Initialize necessary variables, semaphores etc.
+    sem_init(&sem, 0, k);
 
     // Create threads: done for you, do not change
     thread* threads[MAX_THREADS];
@@ -66,5 +83,7 @@ int main() {
         threads[i]->join();
         delete threads[i];
     }
+
+    sem_destroy(&sem);
     return 0;
 }
